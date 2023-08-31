d59d88317074da780e21984848cb91a1079b27d6
Mona Lisa <pranavjayaprakashn98@gmail.com>
2023-08-31 15:33:28 +0530

    update

--- src/Store.js
+++ src/Store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import PrintNumSlice from "./Components/PrintNumSlice";
 import AddSlice from "./Components/AddSlice";
-
+import Datasr from "./Reduxtwo/Datas";
 export const store = configureStore({
     reducer: {
         print:PrintNumSlice,
-        Adder:AddSlice
-    }
-})
+        Adder:AddSlice,
+        datas:Datasr
+    },
+});
+
